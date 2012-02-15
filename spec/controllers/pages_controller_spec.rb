761f29a6957a80aae3690a874e1cac8bf87f414e
Ibexmonj <manjunath.shenoy@gmail.com>
2012-02-14 20:04:49 -0500

    added layout links with routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -46,4 +46,17 @@ end
 	 			:content => "#{@base_title} | About")
  end
   end
+ describe "GET 'help'" do
+    it "returns http success" do
+      get 'help'
+      response.should be_success
+  	end  
+  	 it "should have the title 'Help'" do
+	 get 'help'
+	 response.should have_selector("title", 
+	 			:content => "#{@base_title} | Help")
+ end
+  end
+
+
 end
